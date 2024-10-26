232f4091168882bfcf18a6efdf39e8d240aa4808
daviddaves1 <142112857+daviddaves1@users.noreply.github.com>
2024-10-26 18:25:00 -0300

    Alterei o nome do arquivo e adaptei para mostrar uma mensagem na tela
    Quando o usuário tentar criar a conta, irá aparecer uma mensagem na tela.

--- PetStar/backend/criar-conta-cliente.php
+++ PetStar/backend/criar-conta-cliente.php
@@ -2,7 +2,6 @@
 include 'configuracao.php';
 
 $conn = pg_connect("host=$host dbname=$dbname user=$user password=$password");
-
 if (!$conn) {
     die("Erro de conexão: " . pg_last_error());
 }
@@ -15,20 +14,23 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Verificar se o email já está cadastrado
     $query_email_check = "SELECT * FROM usuarios WHERE email = $1";
     $result_email_check = pg_query_params($conn, $query_email_check, array($email));
-
+    
     if (pg_num_rows($result_email_check) > 0) {
-        echo "Email já cadastrado!<br>";
+        header("Location: ../criar-conta-cliente.html?mensagem=Email já cadastrado! Tente outro email.");
+        exit();
     } else {
         $query = "INSERT INTO usuarios (nome, email, senha) VALUES ($1, $2, $3)";
         $result = pg_query_params($conn, $query, array($nome, $email, password_hash($senha, PASSWORD_DEFAULT)));
-
+        
         if ($result) {
-            echo "Conta criada com sucesso!<br>";
+            header("Location: ../criar-conta-cliente.html?mensagem=Conta criada com sucesso! Volte e faça login.");
+            exit();
         } else {
-            echo "Erro ao criar conta: " . pg_last_error($conn);
+            header("Location: ../criar-conta-cliente.html?mensagem=Erro ao criar conta: " . pg_last_error($conn));
+            exit();
         }
     }
-
+    
     pg_close($conn);
 }
 ?>
